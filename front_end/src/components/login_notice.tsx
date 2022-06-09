432696f19fcb58f77850aab0bab547ef500ca9d2
iceburgy <iceburg2005@hotmail.com>
2022-06-09 13:21:21 -0700

    fix popup twice

--- front_end/src/components/login_notice.tsx
+++ front_end/src/components/login_notice.tsx
@@ -4,7 +4,7 @@ import { Link } from "@mui/material";
 export const LoginNoticeScreen = () => {
     return (
         <CardContent>
-            初次访问，请点击<Link href="#" target="_blank" rel="noopener" onClick={() => {
+            初次访问，请点击<Link href="#" onClick={() => {
                 window.open('https://docs.google.com/document/d/12rgDuEzwhc8OZXU5Whygjwnqqz4xacm0BCqrLF5AsGY/edit?usp=sharing')
             }}>此链接</Link>获取访问密钥
         </CardContent>
