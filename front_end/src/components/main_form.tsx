80b701d4ddc14fe146fd79a2934bd7c343603adc
iceburgy <iceburg2005@hotmail.com>
2022-12-27 23:27:14 -0800

    fix tractor anination

--- front_end/src/components/main_form.tsx
+++ front_end/src/components/main_form.tsx
@@ -986,6 +986,7 @@ export class MainForm {
             } else {
                 showedTractors = showedPoker.GetTractorBySuit(this.tractorPlayer.CurrentHandState.Trump);
             }
+            this.drawingFormHelper.DrawMovingTractorByPosition(showedCards.length, position);
         }
 
         this.RobotPlayFollowing();
